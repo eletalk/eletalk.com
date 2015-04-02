3f61f8328bc4a6032f01424a33b6fadfaac53a4d
Federico Crespo <federico@munett.com>
2015-04-01 19:51:17 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eletalk</title>
+    <link rel="shortcut icon" href="images/icons/favicon.png" />
+    <link href='http://fonts.googleapis.com/css?family=Hind:400,300,600,500,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap & Styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="css/block_grid_bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet" />
+    <link href="css/jquery.circliful.css" rel="stylesheet" />
+    <link href="css/slicknav.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Top Bar-->
+    <div class="top">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="logo">
+                    <a href="index.html"><img src="images/logo.png" alt="" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-9">
+
+                <nav id="desktop-menu">
+                    <ul class="sf-menu" id="navigation">
+                        <li class="current"><a href="index.html#">Inicio</a>
+
+                        </li>
+
+                        <li><a href="nosotros.html">Nosotros</a></li>
+                        <li><a href="contacto.html">Contacto</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- End of Top Bar-->
+
+    <!-- Slider -->
+
+    <div class="slidercontainer">
+        <div id="mainslider" class="owl-carousel">
+
+            <div class="item">
+                <div class="slidecaption">
+                  <h2>LA FORMA MÁS INTELIGENTE DE CONTACTO <br> MARCA-USUARIO.</h2>
+                  <h4>Llega a un mercado de alto poder adquisitivo de forma super efectiva.</h4>
+                  <!-- <p><a class="btn btn-slide" href="index.html#">See the Features</a> -->
+                    </p>
+                </div>
+                <img src="images/slider/1.png" alt="" />
+            </div>
+
+            
+        </div>
+
+
+    </div>
+    <!-- End of Slider -->
+
+    <!--  Features -->
+    <section class="features">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>La mejor forma de conectar tu marca con tu audiencia</h2>
+                <span>Los procedimientos utilizados por eletalk® tienen los resultados más eficientes en captación de usuarios.</span>
+            </div>
+        </div>
+
+        <div class="row spacing-70">
+            <div class="col-sm-4">
+                <div class="feature wow zoomIn" data-wow-delay="0.2s">
+                    <img src="images/webicon1.png" alt="" />
+                    <h4>Llega a más gente más rápido</h4>
+                    <p>Estudios prueban que el modelo eletalk conecta marcas 75% mas efectivamente que publicidad tradicional.</p>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="feature wow zoomIn" data-wow-delay="0.4s">
+                    <img src="images/webicon2.png" alt="" />
+                    <h4>Estamos en los mejores edificios.</h4>
+                    <p>Tenemos presencia en los edificios de más alto nivel llegando así a más de 50,000 ejecutivos diariamente.</p>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="feature wow zoomIn" data-wow-delay="0.6s">
+                    <img src="images/webicon3.png" alt="" />
+                    <h4>La empatía hace la venta</h4>
+                    <p>Eletalk permite mostrar contenido personalizado y en tiempo real, generando así mayor conexión con el usuario.</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End of Features -->
+
+
+
+
+    <!--  Statistics -->
+    <section class="statistics">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Algunas estadísitcas interesantes <i class="fa fa-smile-o"></i></h2>
+                <!-- <p>Synthesize inclusive capitalism, global health clean water grantees nonprofit equal opportunity.</p> -->
+            </div>
+        </div>
+
+        <div class="row spacing-70">
+
+            <div class="col-sm-3">
+                <div class="wow fadeInUp" data-wow-delay="0.1s">
+                    <p id="myStat1" data-dimension="175" data-animationstep="0.5" data-text="+25%" data-width="25" data-fontsize="29" data-percent="30" data-fgcolor="#00adef" data-bgcolor="#E1E3E4" data-fill="#CECECE"></p>
+                    <span>De Elevadores en MTY & DF</span>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="wow fadeInUp" data-wow-delay="0.2s">
+                    <p id="myStat2" data-dimension="175" data-animationstep="0.5" data-text="+50,000" data-width="25" data-fontsize="29" data-percent="80" data-fgcolor="#00adef" data-bgcolor="#E1E3E4" data-fill="#CECECE"></p>
+                    <span>Ejecutivos diarios</span>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="wow fadeInUp" data-wow-delay="0.3s">
+                    <p id="myStat3" data-dimension="175" data-animationstep="0.5" data-text="+65%" data-width="25" data-fontsize="29" data-percent="70" data-fgcolor="#00adef" data-bgcolor="#E1E3E4" data-fill="#CECECE"></p>
+                    <span>Efectividad de anuncios</span>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="wow fadeInUp" data-wow-delay="0.4s">
+                    <p id="myStat4" data-dimension="175" data-animationstep="0.5" data-text="96%" data-width="25" data-fontsize="29" data-percent="96" data-fgcolor="#00adef" data-bgcolor="#E1E3E4" data-fill="#CECECE"></p>
+                    <span>Marcas recurrentes</span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End of Statistics -->
+
+    <!-- <div class="testimonials gray">
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Lo que la prensa habla de nosotros:</h3>
+                <p>La prensa nacional e internacional habla de eletalk®.</p>
+
+                <div id="testimonials-carousel" class="owl-carousel">
+
+                    <div class="item">
+                        <div class="testimonial-content">
+                            <div class="testimonialimg"><img src="images/testimonial1.jpg" alt="" />
+                            </div>
+                            <p> social analysis dignity health developing nations. </p>
+                            <div class="whoclient">
+                                <h5>Alvaro DeMichelis, Founder of <a href="">Global Corp.</a></h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <div class="testimonial-content">
+                            <div class="testimonialimg"><img src="images/testimonial2.jpg" alt="" />
+                            </div>
+                            <p>International tackle working alongside. Economic security, significant invest, Ford Foundation respect public service crowdsourcing research affordable health. Philanthropy equal opportunity assessment expert accelerate United Nations environmental, poverty, political, public.</p>
+                            <div class="whoclient">
+                                <h5>Bjorn Swift, CEO of <a href="">Campaign Ltd.</a></h5>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div class="item">
+                        <div class="testimonial-content">
+                            <div class="testimonialimg"><img src="images/testimonial3.jpg" alt="" />
+                            </div>
+                            <p>Save the world citizens of change; medicine immunize, convener design thinking global network Kickstarter empowerment prevention effectiveness. Service improving quality youth incubation foster. Expanding community ownership, urban experience in the field scalable</p>
+                            <div class="whoclient">
+                                <h5>Bill Good, Chairman of <a href="">Solution S.A.</a></h5>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div> -->
+
+
+    <!--  Footer -->
+    <div class="social">
+        <div class="row">
+
+            <div class="col-sm-6">
+                <ul>
+                    <li><a href="facebook.com/eletalkmx" title="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li><a href="twitter.com/eletalkmx" title="twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-sm-6">
+
+                <div id="mc_embed_signup">
+                    <form class="form-inline validate" action="//audemedia.us7.list-manage.com/subscribe/post?u=b5638e105dac814ad84960d90&amp;id=9345afa0aa" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+                        <div class="row no-gutter">
+                            <div class="col-sm-9">
+                                <input type="email" value="" name="EMAIL" class="form-control" id="mce-EMAIL" placeholder="Recibe las noticias y estudios comerciales eletalk" required>
+                                <div style="position: absolute; left: -5000px;">
+                                    <input type="text" name="b_b5638e105dac814ad84960d90_9345afa0aa" tabindex="-1" value="">
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <input type="submit" value="SUBSCRIMIRME" name="subscribe" id="mc-embedded-subscribe">
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="footer">
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Contacto con edificios.</h4>
+                <ul>
+                    <li><a href="">Quiero eletalk en mi edificio</a>
+                    </li>
+                    <li><a href="">Otros servicios eletalk</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h4>Partners Eletalk.</h4>
+                <ul>
+                    <li><a href="">Quiero revender eletalk</a>
+                    </li>
+                    <li><a href="">Me interesa la tecnología eletalk</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h4>Únete al equipo.</h4>
+                <ul>
+                    <li><a href="">Trabajo en eletalk</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h4>Q&A.</h4>
+                <ul class="questions">
+                    <li><i class="fa fa-phone"></i> +1.186.704.2263</li>
+                    <li><a href=""><i class="fa fa-envelope"></i> MÁNDANOS UN E-MAIL</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!--  End of Footer -->
+<a href="index.html#top" id="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/hoverIntent.js"></script>
+    <script src="js/superfish.min.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.circliful.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/custom.js"></script>
+
+    <script>
+    /* Home Page Slider
+   ========================================================================== */
+    $(document).ready(function() {
+
+
+    var sync1 = $("#mainslider");
+    var sync2 = $("#mainslider-nav");
+
+    sync1.owlCarousel({
+    singleItem : true,
+    slideSpeed : 1000,
+    paginationSpeed: 800,
+    navigation: false,
+    pagination:false,
+    autoPlay:7500,
+    afterAction : syncPosition,
+    responsiveRefreshRate : 200,
+    });
+
+    sync2.owlCarousel({
+    items : 4,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [979,4],
+    itemsTablet : [768,4],
+    itemsMobile : [479,2],
+    pagination:false,
+    responsiveRefreshRate : 100,
+    afterInit : function(el){
+    el.find(".owl-item").eq(0).addClass("synced");
+    }
+    });
+
+    function syncPosition(el){
+    var current = this.currentItem;
+    $("#mainslider-nav")
+    .find(".owl-item")
+    .removeClass("synced")
+    .eq(current)
+    .addClass("synced")
+    if($("#mainslider-nav").data("owlCarousel") !== undefined){
+    center(current)
+    }
+    }
+
+    $("#mainslider-nav").on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).data("owlItem");
+    sync1.trigger("owl.goTo",number);
+    });
+
+    function center(number){
+    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+    var num = number;
+    var found = false;
+    for(var i in sync2visible){
+    if(num === sync2visible[i]){
+    var found = true;
+    }
+    }
+
+    if(found===false){
+    if(num>sync2visible[sync2visible.length-1]){
+    sync2.trigger("owl.goTo", num - sync2visible.length+2)
+    }else{
+    if(num - 1 === -1){
+    num = 0;
+    }
+    sync2.trigger("owl.goTo", num);
+    }
+    } else if(num === sync2visible[sync2visible.length-1]){
+    sync2.trigger("owl.goTo", sync2visible[1])
+    } else if(num === sync2visible[0]){
+    sync2.trigger("owl.goTo", num-1)
+    }
+    }
+
+    });
+
+/* Τestimonials
+   ========================================================================== */
+ $(document).ready(function() {
+        $("#testimonials-carousel").owlCarousel({
+            items: 1,
+            autoPlay: 5000,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [979, 1],
+            itemsTablet: [768, 1]
+        });
+    });
+
+    // ______________ STATS
+jQuery(document).ready(function() {
+$('.statistics').waypoint(function() {
+
+ $('#myStat1').circliful();
+ $('#myStat2').circliful();
+ $('#myStat3').circliful();
+ $('#myStat4').circliful();
+
+}, { offset: 800, triggerOnce: true });
+});
+
+    </script>
+</body>
+
+</html>
